209da1a616a72701ddbbb5daf9abb4866fa1e907
Michał Sadecki <michalsadecki90@gmail.com>
2021-05-27 20:17:32 +0200

    added more lines into Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ public class Main {
   public static void main (String[] args) {
   
     System.out.println("Hello, GitHub!!");
+    System.out.println("Hello, GitHub!!");
+    System.out.println("Hello, GitHub!!");
+    System.out.println("Hello, GitHub!!");
+    System.out.println("Hello, GitHub!!");
     
   }
 
